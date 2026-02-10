e687e8e7b6c636f230723b80880792a818938f30
camilorayoc-hub <camilorayoc@gmail.com>
2026-02-10 21:37:16 +0100

    Configure dependency injection

--- src/GnbBackend.Api/Program.cs
+++ src/GnbBackend.Api/Program.cs
@@ -1,44 +1,34 @@
+using GnbBackend.Api.Infrastructure;
+using GnbBackend.Core.Interfaces;
+using GnbBackend.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<IDataRepository, JsonDataRepository>();
+
+builder.Services.AddSingleton<ICurrencyConverter>(sp =>
+{
+    var repo = sp.GetRequiredService<IDataRepository>();
+    var logger = sp.GetRequiredService<ILogger<CurrencyConverter>>();
+    var rates = repo.GetAllExchangeRatesAsync().GetAwaiter().GetResult();
+    return new CurrencyConverter(rates, logger);
+});
+
+builder.Services.AddScoped<ITransactionService, TransactionService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()){
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
